5a4ebefec58637d3ba44c6ff2a2a8419ea3ce0f4
Gage Erwin <gage.erwin@live.com>
2024-12-03 20:12:28 -0600

    Rust for day 3 part 1

--- Day3/src/main.rs
+++ Day3/src/main.rs
@@ -38,6 +38,22 @@ fn part1(data: String) -> i32 {
     result
 }
 
+// fn part2(data: String) -> i32 {
+//     let mut result: i32 = 0;
+//     let mut start: bool = true;
+//     let info: Vec<&str> = data.split("don't");
+//     let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+//     let num = Regex::new(r"\d+").unwrap();
+//     let values: Vec<&str> = re.find_iter(&data).map(|m| m.as_str()).collect();
+//     for n in values {
+//         let ints: Vec<&str> = num.find_iter(n).map(|m| m.as_str()).collect();
+//         let value1 = ints[0].parse::<i32>().unwrap();
+//         let value2 = ints[1].parse::<i32>().unwrap();
+//         result += value1 * value2;
+//     }
+//     result
+// }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
